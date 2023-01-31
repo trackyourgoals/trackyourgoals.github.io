a6ad32941526fefca6e5a599233a197adb126145
nikhilgovind <nikhil_govind@outlook.com>
2023-01-30 19:33:32 -0800

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -37,3 +37,10 @@ async function retrieveGoals() {
 		console.log('Failed to retrieve the object, with error code: ' + error.message);
 	}
 }
+
+function getCurrentDate(){
+	var date = new Date();
+	var n = date.toDateString();
+	var time = date.toLocaleTimeString();
+	document.getElementById('time').innerHTML = n;
+}
