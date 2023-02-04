55faa5aefef0f517dff6446ed171cdf03337d007
nikhilgovind <nikhil_govind@outlook.com>
2023-02-04 12:31:07 -0800

    updated

--- js/home.js
+++ js/home.js
@@ -1,9 +1,9 @@
-function getCurrentDate(){
-	var date = new Date();
-	var n = date.toDateString();
-	var time = date.toLocaleTimeString();
-	document.getElementById('time').innerHTML = n;
-	fetchCurrentTasks();
+function getCurrentDate() {
+    var date = new Date();
+    var n = date.toDateString();
+    var time = date.toLocaleTimeString();
+    document.getElementById('time').innerHTML = n;
+    fetchCurrentTasks();
 }
 
 async function fetchCurrentTasks() {
@@ -17,14 +17,29 @@ async function fetchCurrentTasks() {
     // Do something with the returned Parse.Object values
     for (let i = 0; i < results.length; i++) {
         const object = results[i];
-        const content = '<div class="col-md-4"><div class="card" style="width: 100%;height: auto;"><div class="card-body"><h5 class="card-title text-left">'
-        +object.get('ename') +'</h5>'+
-        '<div class="row"><div class="col-md-12 text-left"><p class="card-text">'
-        +object.get('edesc')+
-        '</p></div></div><div class="row"><div class="col-md-12 text-left">'+
-        '<p class="card-text">'
-        +object.get('eedate')+
-        '</p></div></div></div></div></div>';
+        const content = '<div class="col-md-4">' +
+                            '<div class="card ' + object.get('ecolor') +'" style="width: 100%;height: auto;">' +
+                                '<div class="card-body">' +
+                                    '<h5 class="card-title text-left">'
+                                        + object.get('ename') +
+                                    '</h5>' +
+                                    '<div class="row">' +
+                                        '<div class="col-md-12 text-left">' +
+                                            '<p class="card-text">'
+                                                + object.get('edesc') +
+                                            '</p>' +
+                                        '</div>' +
+                                    '</div>' +
+                                    '<div class="row">' +
+                                        '<div class="col-md-12 text-left">' +
+                                            '<p class="card-text">'
+                                                + object.get('eedate') +
+                                            '</p>' +
+                                        '</div>' +
+                                    '</div>' +
+                                '</div>' +
+                            '</div>' +
+                        '</div>';
         container.innerHTML += content;
     }
 }
