0de0fd6d72efc79b05afc6888c2bd43b9f48f0e8
nikhilgovind <nikhil_govind@outlook.com>
2023-02-04 20:58:18 -0800

    update

--- js/home.js
+++ js/home.js
@@ -43,3 +43,40 @@ async function fetchCurrentTasks() {
         container.innerHTML += content;
     }
 }
+
+async function fetchAllTasks() {
+    const Task = Parse.Object.extend("Task");
+    const query = new Parse.Query(Task);
+    query.ascending("eedate");
+    const results = await query.find();
+    console.log("Successfully retrieved " + results.length + " scores.");
+    const container = document.getElementById('currentTaskRow');
+    // Do something with the returned Parse.Object values
+    for (let i = 0; i < results.length; i++) {
+        const object = results[i];
+        const content = '<div class="col-md-4">' +
+                            '<div class="card ' + object.get('ecolor') +'" style="width: 100%;height: auto;">' +
+                                '<div class="card-body">' +
+                                    '<h5 class="card-title text-left">'
+                                        + object.get('ename') +
+                                    '</h5>' +
+                                    '<div class="row">' +
+                                        '<div class="col-md-12 text-left">' +
+                                            '<p class="card-text">'
+                                                + object.get('edesc') +
+                                            '</p>' +
+                                        '</div>' +
+                                    '</div>' +
+                                    '<div class="row">' +
+                                        '<div class="col-md-12 text-left">' +
+                                            '<p class="card-text">'
+                                                + object.get('eedate') +
+                                            '</p>' +
+                                        '</div>' +
+                                    '</div>' +
+                                '</div>' +
+                            '</div>' +
+                        '</div>';
+        container.innerHTML += content;
+    }
+}
